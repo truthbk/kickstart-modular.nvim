39aa3dd5df8fb1c0594003d2317c5cab5b361401
Jaime Fullaondo <truthbk@gmail.com>
2025-09-08 16:52:09 +0200

    [init] updating init.lua

--- init.lua
+++ init.lua
@@ -88,7 +88,9 @@ P.S. You can delete this when you're done too. It's your config now! :)
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+
+-- Set up both the traditional leader (for keymaps) as well as the local leader (for norg files)
+vim.g.maplocalleader = ','
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
